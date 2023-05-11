c8619943452b32a56f432161d3989c9d6ca70226
Kilomaster3 <Aliaksei.Hrom-ext@fidor.com>
2023-05-11 16:20:53 +0500

    Handle 500 status

--- app.ts
+++ app.ts
@@ -11,22 +11,27 @@ import {
 import 'dotenv/config';
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/api/users' && req.method === 'GET') {
-    getUsers(req, res);
-  } else if (req.url.match(/\/api\/users\/\w+/) && req.method === 'GET') {
-    const id = req.url.split('/')[3];
-    getUserById(req, res, id);
-  } else if (req.url === '/api/users' && req.method === 'POST') {
-    createUser(req, res);
-  } else if (req.url.match(/\/api\/users\/\w+/) && req.method === 'PUT') {
-    const id = req.url.split('/')[3];
-    updateUser(req, res, id);
-  } else if (req.url.match(/\/api\/users\/\w+/) && req.method === 'DELETE') {
-    const id = req.url.split('/')[3];
-    deleteUser(req, res, id);
-  } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Route not found' }));
+  try {
+    if (req.url === '/api/users' && req.method === 'GET') {
+      getUsers(req, res);
+    } else if (req.url.match(/\/api\/users\/\w+/) && req.method === 'GET') {
+      const id = req.url.split('/')[3];
+      getUserById(req, res, id);
+    } else if (req.url === '/api/users' && req.method === 'POST') {
+      createUser(req, res);
+    } else if (req.url.match(/\/api\/users\/\w+/) && req.method === 'PUT') {
+      const id = req.url.split('/')[3];
+      updateUser(req, res, id);
+    } else if (req.url.match(/\/api\/users\/\w+/) && req.method === 'DELETE') {
+      const id = req.url.split('/')[3];
+      deleteUser(req, res, id);
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Route not found' }));
+    }
+  } catch(err) {
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'No answer from server' }));
   }
 });
 
